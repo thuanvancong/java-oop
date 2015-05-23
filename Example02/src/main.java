c17b00ee573c723860392b2e312200f2f214cecc
Admin <Admin@D20914>
2015-05-23 14:27:19 +0700

    exple2

---
+++ Example02/src/main.java
@@ -0,0 +1,12 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Rectangle rect=new Rectangle(3,4);
+		Rectangle rectb=new Rectangle(5,6);
+		System.out.println("rect a:"+rect.area());
+		System.out.println("rect b:"+rectb.area());
+	}
+
+}
