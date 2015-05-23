5601c82de4f34930314830392dc44e94ff756d3c
Admin <Admin@D20914>
2015-05-23 14:25:58 +0700

    example1

---
+++ Example01/src/CRectangle.java
@@ -0,0 +1,13 @@
+
+public class CRectangle {
+	int x,y;
+	public void setValues(int a,int b)
+	{
+		x=a;
+		y=b;
+	}
+	public int area()
+	{
+		return (x*y);
+	}
+}
---
+++ Example01/src/main.java
@@ -0,0 +1,14 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		CRectangle rect=new CRectangle();
+		CRectangle rectb=new CRectangle();
+		rect.setValues(3, 4);
+		rect.setValues(5, 6);
+		System.out.println("rect a:"+rect.area());
+		System.out.println("rect b:"+rect.area());
+	}
+
+}
