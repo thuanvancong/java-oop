d01fcd65852caeedcf04859f4fb4ef4cbfee48a5
Admin <Admin@D20914>
2015-05-23 14:31:17 +0700

    exple6

---
+++ Example06/src/main.java
@@ -0,0 +1,13 @@
+import java.io.Console;
+
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Rectangle rect=new Rectangle(8, 9);
+		System.out.println("rect a:{0}"+rect.area());
+		return ;
+	}
+
+}
